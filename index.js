c3caf3629c730b5e2d4870edd3c8014af4ab3995
Nate <itsnzte@gmail.com>
2022-06-07 21:04:57 -0700

    Started Code, added start function, and DOM variables

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Hello 
+// Variables
+
+const blankWord = document.getElementById('word');
+const start = document.getElementById('startBtn');
+
+// Score 
+let wins = 0;
+let loses = 0;
+
+const words = ['Javascript', 'HTML', 'CSS', 'Boolean', 'Attributes', 'Functions']
+
+
+start.addEventListener("click", startGame)
+
+
+startGame() {
+  console.log('this works great')
+}
